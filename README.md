ecebcc1f7c09c6939a1613ced3b552fea859f0b7
Matthew Stowe <matthew.stowe@digi.com>
2022-03-12 20:52:02 -0500

    Initial checkin.

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # aiotableauserverclient
-Implementation of tableauserverclient API using asynio
+Implementation of tableauserverclient API using asynio following the API conventions of the `tableauserverclient` library implemented by Tableau.
+
+Using the `/subscriptions` endpoint of Tableau as an example typical usage, per their documentation is as follows.
+
+```
+import tableauserverclient as TSC
+
+tableau_auth = TSC.TableauAuth('USERNAME', 'PASSWORD', site_id='CONTENTURL')
+server = TSC.Server('https://SERVER_URL', use_server_version=True)
+server.auth.sign_in(tableau_auth)
+
+# Create the target (content) of the subscription with its ID and type.
+# ID can be obtained by calling workbooks.get() or views.get().
+target = TSC.Target('c7a9327e-1cda-4504-b026-ddb43b976d1d', 'workbook')
+
+# Store the schedule ID and user ID.
+# IDs can be obtained by calling schedules.get() and users.get().
+schedule_id = 'b60b4efd-a6f7-4599-beb3-cb677e7abac1'
+user_id = '28ce5884-ed38-49a9-aa10-8f5fbd59bbf6'
+
+# Create the new SubscriptionItem object with variables from above.
+new_sub = TSC.SubscriptionItem('My Subscription', schedule_id, user_id, target)
+
+# (Optional) Set other fields. Any of these can be added or removed.
+new_sub.attach_image = False
+new_sub.attach_pdf = True
+new_sub.message = "You have an alert!"
+new_sub.page_orientation = TSC.PDFRequestOptions.Orientation.Landscape
+new_sub.page_size_option = TSC.PDFRequestOptions.PageType.B4
+new_sub.send_if_view_empty = True
+
+# Create the new subscription on the site you are logged in.
+new_sub = server.subscriptions.create(new_sub)
+```
+
+This works perfectly well when running automation, however when integration Tableu interactions into a web server it is preferred to utilize asyncio to ensure throughput.
+
+In such cases an async pattern can be applied when following the above convention and using the async implementation style provided in our example.  Instead of creating the `tableauserverclient.Server` instance used above, in the below you would create an instance of `aiotableauserverclient.TableauClientAsync` allowing you to use the same conventions as above, except introducing `await` into your server interactions.  This library is designed to take full advantage of the `tableauserverclient` package's entities, serder, etc... so that it is only replacing underlying networking operations.
+
+This maintains an almost identical experience to the original library, except introducing `async/await`.
+
+
+url: str, username: str, password: str, site_id: str, api_ver: str
+
+```
+import aiotableauserverclient as TSC
+server = TSC.TableauClientAsync('https://SERVER_URL','USERNAME', 'PASSWORD', site_id='CONTENTURL', api_ver='API_VERSION')
+
+# Initialize the client connection and auth.
+await server.init()
+
+# Create the target (content) of the subscription with its ID and type.
+# ID can be obtained by calling workbooks.get() or views.get().
+target = TSC.Target('c7a9327e-1cda-4504-b026-ddb43b976d1d', 'workbook')
+
+# Store the schedule ID and user ID.
+# IDs can be obtained by calling schedules.get() and users.get().
+schedule_id = 'b60b4efd-a6f7-4599-beb3-cb677e7abac1'
+user_id = '28ce5884-ed38-49a9-aa10-8f5fbd59bbf6'
+
+# Create the new SubscriptionItem object with variables from above.
+new_sub = TSC.SubscriptionItem('My Subscription', schedule_id, user_id, target)
+
+# (Optional) Set other fields. Any of these can be added or removed.
+new_sub.attach_image = False
+new_sub.attach_pdf = True
+new_sub.message = "You have an alert!"
+new_sub.page_orientation = TSC.PDFRequestOptions.Orientation.Landscape
+new_sub.page_size_option = TSC.PDFRequestOptions.PageType.B4
+new_sub.send_if_view_empty = True
+
+# Create the new subscription on the site you are logged in.
+new_sub = await server.subscriptions.create(new_sub)
+
+# Cleanup the client connection/etc
+await server.term()
+```
---
+++ aiotableauserverclient/tableauserverclientasync.py
@@ -0,0 +1,205 @@
+from abc import ABC, abstractmethod
+from typing import Generic, List, TypeVar
+
+import aiohttp
+import tableauserverclient as TSC
+
+T = TypeVar("T")
+
+
+class http:
+    HTTP_HEADER_ACCEPT = "accept"
+    HTTP_HEADER_CONTENT_TYPE = "content-type"
+    HTTP_HEADER_CONTENT_JSON = "application/json"
+    HTTP_HEADER_CONTENT_XML = "application/xml"
+
+
+class TableauClientAsync():
+    def __init__(self, url: str, username: str, password: str, site_id: str, api_ver: str):
+        super().__init__()
+        self.__tableau_auth_body = {
+            "credentials": {
+                "name": username,
+                "password": password,
+                "site": {"contentUrl": site_id},
+            }
+        }
+        self.__tableau_auth_headers = {
+            http.HTTP_HEADER_CONTENT_TYPE: http.HTTP_HEADER_CONTENT_JSON,
+            http.HTTP_HEADER_ACCEPT: http.HTTP_HEADER_CONTENT_JSON,
+        }
+        self.__tableau_client = aiohttp.ClientSession(
+            base_url=f"{url}",
+            raise_for_status=True,
+        )
+        self.__api_ver = api_ver
+
+    @property
+    def subscriptions(self):
+        return TableauSubscriptionsEndpointAsync(self)
+
+    @property
+    def users(self):
+        return TableauUsersEndpointAsync(self)
+
+    async def refresh_auth(self):
+        async with self.__tableau_lock.reader_lock:
+            response = await self.__tableau_client.post(
+                self.__get_url("/auth/signin"),
+                json=self.__tableau_auth_body,
+                headers=self.__tableau_auth_headers,
+            )
+        self.__tableau_auth_response = await response.json()
+
+    async def init(self):
+        await self.refresh_auth()
+
+    async def term(self):
+        await self.__tableau_client.close()
+
+    async def get_request(self, url: str):
+        return await self.__tableau_client.get(
+            self.__get_site_url(url), headers=self.__get_headers()
+        )
+
+    async def delete_request(self, url: str):
+        return await self.__tableau_client.delete(
+            self.__get_site_url(url), headers=self.__get_headers()
+        )
+
+    async def post_request(self, url: str, body: str):
+        return await self.__tableau_client.post(
+            self.__get_site_url(url),
+            headers=self.__get_headers(),
+            data=body,
+        )
+
+    async def put_request(self, url: str, body: dict):
+        return await self.__tableau_client.put(
+            self.__get_site_url(url),
+            headers=self.__get_headers(),
+            data=body,
+        )
+
+    def __get_headers(self):
+        return {
+            "X-Tableau-Auth": self.__tableau_auth_response["credentials"]["token"],
+            http.HTTP_HEADER_CONTENT_TYPE: http.HTTP_HEADER_CONTENT_XML,
+            http.HTTP_HEADER_ACCEPT: http.HTTP_HEADER_CONTENT_XML,
+        }
+
+    def __get_site_url(self, url: str):
+        return self.__get_url(
+            f"/sites/{self.__tableau_auth_response['credentials']['site']['id']}{url}"
+        )
+
+    def __get_url(self, url: str):
+        return f"/api/{self.__api_ver}{url}"
+
+
+class BaseTableauEndpointAsync(ABC, Generic[T]):
+    def __init__(self, tableau_client: TableauClientAsync, endpoint: str):
+        super().__init__()
+        self._tableau_client = tableau_client
+        self._endpoint = endpoint
+        self._namespace = {"t": "http://tableau.com/api"}
+
+    @abstractmethod
+    def _from_response(self, data: str) -> List[T]:
+        pass
+
+    @abstractmethod
+    def _to_post_request(self, entity: T):
+        pass
+
+    @abstractmethod
+    def _to_put_request(self, entity: T):
+        pass
+
+    @abstractmethod
+    def _get_entity_id(self, entity: T) -> str:
+        pass
+
+    @abstractmethod
+    def _apply_req_options(self, req: TSC.RequestOptions):
+        pass
+
+    async def get(self, req: TSC.RequestOptions) -> List[T]:
+        self._apply_req_options(req)
+        query = "&".join(
+            [f"{k}={v}" for k, v in req.get_query_params().items()])
+        response = await self._tableau_client.get_request(f"{self._endpoint}?{query}")
+        return self._from_response(await response.text())
+
+    async def get_by_id(self, id: str) -> T:
+        response = await self._tableau_client.get_request(f"{self._endpoint}/{id}")
+        return self._from_response(await response.text())[0]
+
+    async def update(self, entity: T) -> T:
+        response = await self._tableau_client.put_request(
+            f"{self._endpoint}/{self._get_entity_id(entity)}",
+            self._to_put_request(entity),
+        )
+        return self._from_response(await response.text())[0]
+
+
+class TableauUsersEndpointAsync(BaseTableauEndpointAsync[TSC.UserItem]):
+    def __init__(self, tableau_client: TableauClientAsync):
+        super().__init__(tableau_client, "/users")
+
+    def _apply_req_options(self, req: TSC.RequestOptions):
+        req._all_fields = True
+
+    def _get_entity_id(self, entity: TSC.UserItem) -> str:
+        return entity.id
+
+    def _from_response(self, data: str) -> List[TSC.UserItem]:
+        return TSC.UserItem.from_response(data, self._namespace)
+
+    def _to_post_request(self, entity) -> TSC.server.request_factory.UserRequest:
+        return TSC.server.RequestFactory.User.add_req(entity)
+
+    def _to_put_request(self, entity) -> TSC.server.request_factory.UserRequest:
+        return TSC.server.RequestFactory.User.update_req(entity, password=None)
+
+    async def remove(self, id: str) -> None:
+        await self._tableau_client.delete_request(f"{self._endpoint}/{id}")
+
+    async def add(self, user_item: TSC.UserItem) -> TSC.UserItem:
+        response = await self._tableau_client.post_request(
+            self._endpoint, self._to_post_request(user_item)
+        )
+        return self._from_response(await response.text())[0]
+
+
+class TableauSubscriptionsEndpointAsync(BaseTableauEndpointAsync[TSC.SubscriptionItem]):
+    def __init__(self, tableau_client: TableauClientAsync):
+        super().__init__(tableau_client, "/subscriptions")
+
+    def _apply_req_options(self, req: TSC.RequestOptions):
+        pass  # Skip, nothing to do here
+
+    def _get_entity_id(self, entity: TSC.SubscriptionItem) -> str:
+        return entity.id
+
+    def _from_response(self, data: str) -> List[TSC.SubscriptionItem]:
+        return TSC.SubscriptionItem.from_response(data, self._namespace)
+
+    def _to_post_request(
+        self, entity
+    ) -> TSC.server.request_factory.SubscriptionRequest:
+        return TSC.server.RequestFactory.Subscription.create_req(entity)
+
+    def _to_put_request(self, entity) -> TSC.server.request_factory.SubscriptionRequest:
+        return TSC.server.RequestFactory.Subscription.update_req(entity)
+
+    async def delete(self, id: str) -> None:
+        await self._tableau_client.delete_request(f"{self._endpoint}/{id}")
+
+    async def create(
+        self, subscription_item: TSC.SubscriptionItem
+    ) -> TSC.SubscriptionItem:
+        response = await self._tableau_client.post_request(
+            self._endpoint, self._to_post_request(subscription_item)
+        )
+        return self._from_response(await response.text())[0]
